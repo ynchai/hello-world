90afd88a4f83a3ae800d0597821e09f8067db85b
ynchai <ynchai@126.com>
2018-07-16 10:37:35 +0800

    修改和远程新分支对应的本地分支

---
+++ HelloWorld/HelloWorld/TestCtrl1.m
@@ -0,0 +1,37 @@
+//
+//  TestCtrl1.m
+//  HelloWorld
+//
+//  Created by lyj on 2018/7/16.
+//  Copyright © 2018年 cyn. All rights reserved.
+//
+
+#import "TestCtrl1.h"
+
+@interface TestCtrl1 ()
+
+@end
+
+@implementation TestCtrl1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
